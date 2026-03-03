c3d738088c90055d8ebec92b013a53f71e46ebf5
Agnieszka Barcik <aga.barcik11@gmail.com>
2026-03-03 16:42:53 -0500

    Fix review comments: type hint, formatting and bite logic

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Animal:
-    alive = []
+    alive: list["Animal"] = []
 
     def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
@@ -8,12 +8,18 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
 
 class Herbivore(Animal):
 
     def hide(self) -> None:
-         self.hidden = not self.hidden
+        self.hidden = not self.hidden
+
 
 class Carnivore(Animal):
 
